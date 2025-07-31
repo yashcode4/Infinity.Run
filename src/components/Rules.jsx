f02f25a4a9175c69712ccb65a32d6d2c98cda1f2
yashcode4 <yash124987@gmail.com>
2025-07-31 22:32:24 +0530

    rules: update rules section to clarify value-based input and mode switching

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -2,14 +2,14 @@ import pi_img from "../images/mode-images/pi_img.png";
 import netflix_img from "../images/mode-images/netflix_img.png";
 import marvel_img from "../images/mode-images/marvel_img.png";
 import dc_img from "../images/mode-images/dc_img.png";
-import anime_img from "../images/mode-images/anime_img.png";  
+import anime_img from "../images/mode-images/anime_img.png";
 
 const Rules = () => {
   return (
     <>
       <div className="layout section">
         <h1>Rules</h1>
-        <p>Player will jump when you press the correct input that matches the respective mode's input. There are a total of 10 modes in the game, each with its own corresponding input.</p>
+        <p>The player will jump when you press the correct value that matches the current mode's value. There are 11 modes in total, each with its own set of values. You can switch between modes by clicking the mode icon, which updates the value type accordingly.</p>
 
         <div className="img-group">
           <img src={pi_img} alt="pi_img" />
